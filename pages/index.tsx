958dd2e6a496f8e7a0c2520cf4a655f4a79bbc10
Davi Suga <daviciencia1@gmail.com>
2021-02-27 13:41:08 -0300

    feat(homepage): added server side voucher fetching
    Implemented products and vouchers data serialization in the server-side to make front-end data
    operations faster and easier to deal with.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,39 @@
 import HomePage from "../components/HomePage";
 import api from "../services/axios";
 
-export default function Home({ products, error }) {
-  return <HomePage {...{ products, error }} />;
+export default function Home({ products, error, vouchers }) {
+  return <HomePage {...{ products, error, vouchers }} />;
 }
 
 export async function getStaticProps() {
   try {
-    const products = await api.get("products.json");
-    return { props: { products: products.data.products } };
+    const productsPromise = api.get<{ products: ProductProps[] }>(
+      "products.json"
+    );
+    const voucherPromise = api.get<{ vouchers: Voucher[] }>("vouchers.json");
+    const [products, vouchers] = await Promise.all([
+      productsPromise,
+      voucherPromise,
+    ]);
+
+    const productsData = products.data.products.reduce((acc, product) => {
+      return {
+        ...acc,
+        [product.name]: { price: product.price, available: product.available },
+      };
+    }, {});
+    const vouchersData = vouchers.data.vouchers.reduce((acc, voucher) => {
+      return {
+        ...acc,
+        [voucher.code]: {
+          type: voucher.type,
+          minValue: voucher.minValue || null,
+          amount: voucher.amount,
+        },
+      };
+    }, {});
+    debugger;
+    return { props: { products: productsData, vouchers: vouchersData } };
   } catch (error) {
     return {
       props: {
