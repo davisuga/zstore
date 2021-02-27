703a35fe0ea652b3c851d9487297d4b0de910b5e
Davi Suga <daviciencia1@gmail.com>
2021-02-27 19:45:56 -0300

    feat(product item): added add/remove/show item feature
    Created basic component structure and passed callbacks via props to modify external state.

--- components/ShoppingCart/ProductItem/index.tsx
+++ components/ShoppingCart/ProductItem/index.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import Flex from "../../Flex";
 
-// import { Container } from './styles';
+import {
+  Container,
+  ProductPicture,
+  ProductInfo,
+  ProductName,
+  ProductQuantity,
+  QuantityController,
+  Add,
+  Remove,
+  ProductPrice,
+} from "./styles";
 
-const ProductItem: React.FC = () => {
-  return <div />;
+type ProductItemProps = {
+  product: ProductProps;
+  onAddProduct: (product: ProductProps) => void;
+  onRemoveProduct: (product: ProductProps) => void;
+};
+
+const ProductItem: React.FC<ProductItemProps> = ({
+  product,
+  onAddProduct,
+  onRemoveProduct,
+}) => {
+  return (
+    <Container>
+      <ProductPicture />
+      <ProductInfo>
+        <ProductName>{product.name}</ProductName>
+        <Flex flexDirection="row" justifyContent="space-between">
+          <ProductQuantity>Quantity: {product.quantity}</ProductQuantity>
+          <ProductPrice>${product.price * product.quantity}</ProductPrice>
+        </Flex>
+      </ProductInfo>
+      <QuantityController>
+        <Add onClick={() => onAddProduct(product)}>+</Add>
+        <Remove onClick={() => onRemoveProduct(product)}>-</Remove>
+      </QuantityController>
+    </Container>
+  );
 };
 
 export default ProductItem;
